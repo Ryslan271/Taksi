5ce088864bb1d7538ad7e4cbc0d1b61135b91165
Ruslan <Ruslan@DESKTOP-NR8DVE4>
2022-12-19 23:57:46 +0300

    Добавил переход на след строчку для TextBox Правильно настроел ComboBox

--- Taksi/Pages/MakeOrderPage.xaml.cs
+++ Taksi/Pages/MakeOrderPage.xaml.cs
@@ -69,6 +69,9 @@ namespace Taksi.Pages
 
         private void Address_TextChanged(object sender, TextChangedEventArgs e)
         {
+            if (MakeOrder == null)
+                return;
+
             TimeSpan dateTime = MakeOrder.TimeForExecution;
 
             Cost += (DateTime.Now - dateTime).Hour * 100;
